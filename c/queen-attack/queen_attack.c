cdf75470930057021f9173cf88d7b9e292e61342
tanakornsss <omtanakorn2550@gmail.com>
2025-06-18 19:39:20 +0700

    Commit from PC

--- c/queen-attack/queen_attack.c
+++
@@ -1,5 +0,0 @@
-#include "queen_attack.h"
-
-attack_status_t can_attack(position_t queen_1, position_t queen_2) {
-	return CAN_ATTACK; // placeholder
-}
--- c/resistor-color/resistor_color.c
+++ c/resistor-color/resistor_color.c
@@ -1,5 +1,15 @@
 #include "resistor_color.h"
 
 uint16_t color_code(resistor_band_t color) {
-	return (uint16_t) color;
+	return color;
+}
+
+const resistor_band_t* colors(void) {
+	 static const resistor_band_t expected[] = { BLACK,
+		 BROWN, RED, ORANGE, YELLOW,
+                 GREEN, BLUE,  VIOLET, GREY,   WHITE 
+	 };
+
+	 return expected;
+ 
 }
