59fe336f58c9a4b7020821e1f89d5cd128ac3029
tanakornsss <omtanakorn2550@gmail.com>
2025-06-19 14:17:07 +0700

    Implement check function

--- c/queen-attack/queen_attack.c
+++ c/queen-attack/queen_attack.c
@@ -1,24 +1,26 @@
 #include "queen_attack.h"
 
 attack_status_t can_attack(position_t queen_1, position_t queen_2) {
-	
-	// Not-null check
-	if (queen_1.row < 1 ||
-		queen_1.column < 1 ||
-	       	queen_2.row < 1 ||
-	       	queen_2.column < 1
-	) return INVALID_POSITION;	
+	if (initNotOutOfBoundCheck(queen_1, queen_2)) return INVALID_POSITION;
+	if (checkNotSamePos(queen_1, queen_2)) return INVALID_POSITION;
 
-	// Return INVALID_POSITION if the queen_1 or queen_2 is out of range
-	if (queen_1.row > OUT_OF_RANGE_TILES ||
-		queen_1.column < OUT_OF_RANGE_TILES || 
-		queen_2.row < OUT_OF_RANGE_TILES ||
-	       	queen_2.column > OUT_OF_RANGE_TILES
-	) return INVALID_POSITION;
-	
-	// Return INVALID_POSITION if same the queen_1 and queen_2 is the same pos
-	if ((queen_1.row == queen_2.row) && 
-		(queen_1.column == queen_2.column)) return INVALID_POSITION;
+	return UNDEFINED_ERROR_CODE;
+}
 
-	return INVALID_POSITION;
+bool initNotOutOfBoundCheck(position_t queen_1, position_t queen_2) {
+	if (	
+		queen_1.column > OUT_OF_RANGE_TILES ||
+		queen_1.row > OUT_OF_RANGE_TILES ||
+		queen_2.column > OUT_OF_RANGE_TILES ||
+		queen_2.row > OUT_OF_RANGE_TILES
+	) return true;
+	return false;	
+}
+
+bool checkNotSamePos(position_t queen_1, position_t queen_2) {
+	if (
+		queen_1.column == queen_2.column &&
+		queen_1.row == queen_2.row		
+	) return true;
+	return false;
 }
--- c/queen-attack/queen_attack.h
+++ c/queen-attack/queen_attack.h
@@ -2,8 +2,10 @@
 #define QUEEN_ATTACK_H
 
 #define OUT_OF_RANGE_TILES 7
+#define UNDEFINED_ERROR_CODE 1000 
 
 #include <stdint.h>
+#include <stdbool.h>
 
 typedef enum { CAN_NOT_ATTACK, CAN_ATTACK, INVALID_POSITION } attack_status_t;
 
@@ -13,5 +15,8 @@ typedef struct {
 } position_t;
 
 attack_status_t can_attack(position_t queen_1, position_t queen_2);
+
+bool initNotOutOfBoundCheck(position_t queen_1, position_t queen_2);
+bool checkNotSamePos(position_t queen_1, position_t queen_2);
 
 #endif
